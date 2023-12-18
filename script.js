79816981c35e15c4f9381ee2b94f262fe59f57d5
shekhargrace14 <82882207+shekhargrace14@users.noreply.github.com>
2023-12-18 11:46:15 +0000

    ok

--- script.js
+++ script.js
@@ -1,23 +1,71 @@
-// d05c0f50238a42a6b2b95625231612 
+// c182d4eda330bc791453f992d1788091
+
 
 const button = document.getElementById("search-button");
 const input = document.getElementById("city-input")
 
-async function  getData(cityName){
-    const promise = await fetch(`http://api.weatherapi.com/v1/current.json?key=d05c0f50238a42a6b2b95625231612&q=${cityName}&aqi=no`); 
-    return await promise.json()
+
+
+
+
+// fetch("https://api.openweathermap.org/data/2.5/weather?q={input}&appid=c182d4eda330bc791453f992d1788091")
+// .then((result)=>{ return result.json()})
+// .then((data)=>{
+//     console.log(data)
+    
+// })
+
+
+async function weatherApi(result="delhi"){
+    
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${result}&appid=c182d4eda330bc791453f992d1788091`);
+
+    var data = await response.json();
+
+    // return data
+    console.log(data);
+
+                const state = document.querySelector(".state");
+                const time = document.querySelector(".time");
+                const temp = document.querySelector(".temp");
+
+                state.innerHTML = data.name;
+                time.innerHTML = data.timezone;
+                temp.innerHTML = data.main.temp;        
 } 
 
-console.log(getData())
 
-// button.addEventListener("click", async function(){
-//     const value = input.value
-//     const result = await getData(value)
-//     console.log( result) 
+weatherApi()
+button.addEventListener("click", function(){
+    result = input.value
+    console.log(result)
+    weatherApi(result)
+
+})
 
 
-//     // input.innerHTML = ""
 
-// })
-console.log("hello")
 
+
+
+        // button.addEventListener("click", () => {
+        //     const input = document.getElementById("city-input").value;
+        //     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=c182d4eda330bc791453f992d1788091`)
+        //     .then((res)=> res.json())
+        //     .then((data) => {
+        //         console.log(data);
+        //         console.log(data.base)
+
+        //         const state = document.querySelector(".state");
+        //         const time = document.querySelector(".time");
+        //         const temp = document.querySelector(".temp");
+
+        //         state.innerHTML = data.name;
+        //         time.innerHTML = data.timezone;
+        //         temp.innerHTML = data.main.temp;
+                
+        //     })
+
+
+        //     console.log("hello", input)
+        // })
